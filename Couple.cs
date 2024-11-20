e971dea22256c434323af09bbd31107cbc226af8
iris_mechain <iris.mechain@gmail.com>
2024-11-20 09:16:44 +0100

    Ajout et test de la classe Couple

---
+++ Couple.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpectreAmplitude
+{
+    internal class Couple
+    {
+        // assesseurs
+        public int FirstValue { get; set; }
+        public double SecondValue { get; set; }
+
+        //constructeur
+        public Couple(int firstValue, double secondValue)
+        {
+            FirstValue = firstValue;
+            SecondValue = secondValue;
+        }
+
+        public override string ToString()
+        {
+            return $"({FirstValue}, {SecondValue})";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -10,6 +10,21 @@ namespace SpectreAmplitude
     {
         static void Main(string[] args)
         {
+            // Créer une liste pour stocker les objets Couple
+            List<Couple> couples = new List<Couple>();
+
+            // Ajouter des couples (1/3, 3), (1/5, 5) etc.
+            for (int i = 3; i <= 5; i += 2)
+            {
+                double secondValue = 1.0 / i;
+                couples.Add(new Couple(i, secondValue));
+            }
+
+            // Afficher les valeurs
+            foreach (var couple in couples)
+            {
+                Console.WriteLine(couple);
+            }
         }
     }
 }
